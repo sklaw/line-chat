d6329d2c5db6f38b9a6c207cd843bbe4c4771bac
sklaw <271462243@qq.com>
2015-01-08 11:55:56 +0800

    change the path to python 2.7 from 2.6

--- diy/hellotornado.py
+++ diy/hellotornado.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 cwd = os.path.dirname(os.path.abspath(__file__))
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(cwd, '..', 'misc/virtenv/lib/python2.6/site-packages')
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(cwd, '..', 'misc/virtenv/lib/python2.7/site-packages')
 virtualenv = os.path.join(cwd, '..', 'misc/virtenv/bin/activate_this.py')
 execfile(virtualenv, dict(__file__=virtualenv))
 
