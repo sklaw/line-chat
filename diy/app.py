4e249e23ebf9bbe1578d9c91e8b31fa07830170a
sklaw <271462243@qq.com>
2015-01-08 11:15:02 +0800

    asyncmongo test 1

--- diy/app.py
+++ diy/app.py
@@ -3,24 +3,28 @@ import tornado.web
 import tornado.httpserver
 import pymongo
 import os
-
-
+import asyncmongo
+import tornado.gen
 
 class Application(tornado.web.Application):
     def __init__(self):
-        handlers = [(r"/(\w+)", WordHandler)]
-        conn = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
-        self.test3 = conn.test3
-        self.cl_1 = self.test3.cl_1
-        self.cl_1.insert({"name":"fuck", "def":"have sex with"})
+        handlers = [(r"/.*", WordHandler)]
+        self.test3 = asyncmongo.Client(pool_id="mydb", host=os.environ['OPENSHIFT_MONGODB_DB_HOST'], port=os.environ['OPENSHIFT_MONGODB_DB_PORT'], dbname='test3')
+        
+        
         tornado.web.Application.__init__(self, handlers)
 
-
 class WordHandler(tornado.web.RequestHandler):
-    def get(self, word):
-        tmp = self.application.cl_1.find_one({"name":"fuck"})
+
+    @tornado.web.asynchronous
+    @tornado.gen.engine
+    def get(self):
+        self.cl_1 = self.application.test3.cl_1
+        ( yield tornado.gen.Task(self.cl_1.insert, {"name":"fuck", "def":"have sex with"}) )
+        tmp = ( yield tornado.gen.Task(self.cl_1.find_one, {"name":"fuck"}) )[0][0]
         del tmp["_id"]
         self.write(tmp)
+        self.finish()
 
 def main(address):
     http_server = tornado.httpserver.HTTPServer(Application())
