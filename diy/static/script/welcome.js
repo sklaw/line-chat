c241a89c95266b8bd120e7391c812148583aa11b
sklaw <271462243@qq.com>
2015-01-10 23:12:01 +0800

    4th try

--- diy/static/script/welcome.js
+++ diy/static/script/welcome.js
@@ -41,7 +41,7 @@ function draw(event) {
 }
 
 function wbstart() {
-	var host = "ws://test3-sklaw.rhcloud.com/share";
+	var host = "https://test3-sklaw.rhcloud.com/share";
 	websocket = new WebSocket(host);
 	websocket.onopen = function (evt) {
 		$("#result").html("wb opened.")
