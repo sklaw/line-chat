b7f50675c0a933ce456e1fda8792ce0b27a47a37
sklaw <271462243@qq.com>
2015-01-12 03:34:15 +0800

    save self lines 6

--- diy/static/script/welcome.js
+++ diy/static/script/welcome.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
           		var event = {};
           		event.offsetX = x
           		event.offsetY = y
-          		//$("#cord").html(event.offsetX.toString() + ", " + event.offsetY.toString())
+          		$("#cord").html(event.offsetX.toString() + ", " + event.offsetY.toString())
           		oldx = event.offsetX
   				oldy = event.offsetY
           		setTimeout(draw, 100, event)
